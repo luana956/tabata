7c5e6f3f19f002bb2ef38c458077d76dab6c9019
luana956 <luana.hipolito.pereira@escola.pr.gov.br>
2026-09-21 09:32:22 -0300

    Add button click event handling
    Fix event listener typo and update button click handling.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+   const botoes= document.querySelector("button");
 
+      botoes.forEach(function (botao) { 
+          let curtiu = false;
+          botao.addEventlistener("click", botaoclicado);
+          function botaoClica() {
+            console.log("fui clicado");
+            let texto = botao.querySelector("span");
+            if (curtiu === false) {
+           
+         }
+        texto.textContent++;
+          }
+      });    
+      function botaoClicado() {
+        console.log("fui clicado"); 
+        let texto = botao.querySelector("span");
+        texto.textContent++;
+      }
